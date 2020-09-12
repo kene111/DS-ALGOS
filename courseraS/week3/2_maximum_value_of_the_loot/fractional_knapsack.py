0f0cdbe1136e99ab0ff0b9f6ed2cc0c25a28bf2d
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-09-12 04:41:55 +0100

    updated knapsack solution

--- courseraS/week3/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ courseraS/week3/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -2,39 +2,58 @@
 import sys
 from time import time
 
-def get_optimal_value(capacity, weights, values):
-    #value = 0.
-    # write your code here
+def get_optimal_value_0(capacity, weights, values):
+
     value_weights =  zip(values, weights)
     weights_value_dict = {w:v for v,w in value_weights}
     units = [weights_value_dict[w]/w for w in weights_value_dict]
     units_weights =  zip(units, weights)
     units_weights_dict = {u:w for u,w in units_weights}
-    units = sorted(units, reverse=True)
-    knapsack = []
-    value_sum = []
+    units.sort(reverse=True)
+    value_sum = 0
     i = 0
-    v = 0
-    check = 0
+    n = len(units)
 
-
-    while True:
-        if sum(knapsack) == capacity:
-            return(sum(value_sum))
-
+    while capacity != 0 and i < n:
+       
         if units_weights_dict[units[i]] <= capacity:
-            knapsack.append(units_weights_dict[units[i]])
-            value_sum.append(weights_value_dict[units_weights_dict[units[i]]])
-            i += 1
+            value_sum += weights_value_dict[units_weights_dict[units[i]]]
+            capacity -= units_weights_dict[units[i]]
+            i += 1 
 
-        if units_weights_dict[units[i]] > capacity:
-            v += units[i]
-            check += 1
-            if check == capacity:
-                return(v)
+        elif units_weights_dict[units[i]] > capacity:
+            value_sum += capacity * units[i] 
+            capacity = 0
 
             
-    #return value
+                     
+    return value_sum 
+
+
+def get_optimal_value_1(capacity, weights, values):
+   
+    units = [values[i]/weights[i] for i in range(len(weights))]
+    units_weights_values =  list(zip(units, weights, values))
+    units_weights_values.sort(key=lambda x: x[0], reverse= True)
+    value_sum = 0
+    i = 0
+    n = len(units)
+
+    while capacity != 0 and i < n:
+        unit, weight, value = units_weights_values[i]
+       
+        if weight <= capacity: 
+            value_sum += value 
+            capacity -= weight 
+    
+        elif weight > capacity: 
+            value_sum += capacity * unit
+            capacity = 0
+
+        i += 1
+            
+                     
+    return value_sum #sum(value_sum)
 
 
 if __name__ == "__main__":
@@ -43,6 +62,6 @@ if __name__ == "__main__":
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     #t0 = time()
-    opt_value = get_optimal_value(capacity, weights, values)
+    opt_value = get_optimal_value_1(capacity, weights, values)
     print("{:.10f}".format(opt_value))
     #print(time()-t0)
