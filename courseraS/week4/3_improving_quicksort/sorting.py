54ff483dc2442db539d4b8e1522eaee818b86dfe
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-09-24 16:43:38 +0100

    optimised random quick-sort

--- courseraS/week4/3_improving_quicksort/sorting.py
+++ courseraS/week4/3_improving_quicksort/sorting.py
@@ -12,34 +12,36 @@ def partition3(a, l, r):
         if  a[i] <= x: 
             j += 1
             a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]          
-    
-    
-    curr = j
+    a[l], a[j] = a[j], a[l] 
 
-    for i in range(l,j):
-        if a[i] == a[j]:
-            if a[j] - a[i] == 0:
-                return curr, j
+    #print(a)         
+    
+    main = j
+    curr = j
+    for i in range(l, j):
+        if a[i] == a[j] and i < j:
             curr -= 1
             a[i],a[curr] = a[curr], a[i]
-    return curr, j
+            j -= 1
 
+                   
+    return curr, main
 
-    '''
+''' This while loop works perfectly but still isn't as fast enough for very large arrays
+
     while l < j:
         if a[l] == a[j]:
-            if a[j] - a[l] == 0:# and a[l] == a[j]:
+            if a[j] - a[l] == 0:
                 return curr, j
             curr -= 1
             a[l],a[curr] = a[curr], a[l]
         l += 1
     return curr, j
-    '''
 
+'''
 
     
-
+'''
 def partition2(a, l, r):
     x = a[l]
     j = l
@@ -49,7 +51,7 @@ def partition2(a, l, r):
             a[i], a[j] = a[j], a[i]
     a[l], a[j] = a[j], a[l]
     return j
-
+'''
 
 def randomized_quick_sort(a, l, r):
     if l >= r:
@@ -61,8 +63,15 @@ def randomized_quick_sort(a, l, r):
     #m = partition2(a, l, r)
     randomized_quick_sort(a, l, d - 1);
     randomized_quick_sort(a, u + 1, r);
+'''
+q = [5,5,4,3,2,1]
+#q = [9,2,2,9,3,9,6,7,9]
+w = len(q) - 1
 
+print(partition3(q,0,w))
+print(q)
 
+'''
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
@@ -71,7 +80,7 @@ if __name__ == '__main__':
     for x in a:
         print(x, end=' ')
     #print(' ')
-    #print(time() - t0)
+   # print(time() - t0)
 
 
 
